b08b18364c6ed4fd9c2cd8fce9a4b022d82f62d1
webdev-web2go <desarrollos.web2go@gmail.com>
2024-04-18 12:30:36 -0600

    Change base url

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,14 +10,14 @@ export const metadata: Metadata = {
     "En MTC, nos dedicamos a ofrecer soluciones integrales para tus necesidades de abastecimiento y servicios comerciales. Con años de experiencia en el mercado, nos enorgullece ser líderes en la industria, brindando productos de calidad y servicios confiables a nuestros clientes en todo momento.",
   openGraph: {
     type: "website",
-    url: "https://macromtc.vercel.app/",
+    url: "https://www.macromtc.com/",
     siteName: "MTC | Macrotráfico Comercial",
     title: "MTC | Macrotráfico Comercial",
     description:
       "En MTC, nos dedicamos a ofrecer soluciones integrales para tus necesidades de abastecimiento y servicios comerciales. Con años de experiencia en el mercado, nos enorgullece ser líderes en la industria, brindando productos de calidad y servicios confiables a nuestros clientes en todo momento.",
     images: [
       {
-        url: "https://macromtc.vercel.app/hero/hero1.webp",
+        url: "https://www.macromtc.com/hero/hero1.webp",
       },
     ],
   },
