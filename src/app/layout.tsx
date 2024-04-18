13b41ce792079367d7bbbd58da449ec6dff657c9
webdev-web2go <desarrollos.web2go@gmail.com>
2024-04-18 10:31:45 -0600

    Add open graph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,18 +8,19 @@ export const metadata: Metadata = {
   title: "MTC | Macrotráfico Comercial",
   description:
     "En MTC, nos dedicamos a ofrecer soluciones integrales para tus necesidades de abastecimiento y servicios comerciales. Con años de experiencia en el mercado, nos enorgullece ser líderes en la industria, brindando productos de calidad y servicios confiables a nuestros clientes en todo momento.",
-  // openGraph: {
-  //   type: "website",
-  //   url: "https://serviciosprocargodelsureste.vercel.app/",
-  //   siteName: "Servicios Procargo del Sureste",
-  //   title: "Servicios Procargo del Sureste",
-  //   description: "Soluciones logísticas para tus necesidades.",
-  //   images: [
-  //     {
-  //       url: "https://serviciosprocargodelsureste.vercel.app/hero/og_image.webp",
-  //     },
-  //   ],
-  // },
+  openGraph: {
+    type: "website",
+    url: "https://macromtc.vercel.app/",
+    siteName: "MTC | Macrotráfico Comercial",
+    title: "MTC | Macrotráfico Comercial",
+    description:
+      "En MTC, nos dedicamos a ofrecer soluciones integrales para tus necesidades de abastecimiento y servicios comerciales. Con años de experiencia en el mercado, nos enorgullece ser líderes en la industria, brindando productos de calidad y servicios confiables a nuestros clientes en todo momento.",
+    images: [
+      {
+        url: "https://macromtc.vercel.app/hero/hero1.webp",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
